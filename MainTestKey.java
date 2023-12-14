236cebc07e59902c8919bc29292346d9cbb2f4f0
VitDiDev <venn.dev15@gmail.com>
2023-12-14 19:44:08 +0700

    Version tối

--- MainTestKey.java
+++ MainTestKey.java
@@ -1,15 +1,15 @@
 import java.io.IOException;
 import java.util.List;
 
-public class main {
+public class MainTestKey {
     public static void main(String[] args) {
         try {
             // Generate test data using the DataGenerator
-            DataGenerator data = new DataGenerator();
-            data.generateTestData("cities.csv");
+            // DataGenerator data = new DataGenerator();
+            // data.generateTestData("cities.csv");
             
             // Read data from the generated file
-            String fileName = "cities.csv";
+            String fileName = "cities_testdapan.csv";
             List<String> lines = FileReader.readLines(fileName);
 
             // Check if the file is empty
@@ -17,18 +17,19 @@ public class main {
                 System.out.println("File is empty!");
             } else {
                 // Set the number of iterations, cities, and population size
-                int iterations = 100;
-                int cities = lines.size() -1;
-                int populationSize = lines.size() -1;
-                // int populationSize = 5;
+                int iterations = 5;
+                int cities = lines.size() -1 ;
+
                 System.out.println(cities);
+                // int populationSize = lines.size();
+                int populationSize = 5;
 
 
                 // Generate random probabilities for crossover and mutation
-                double crossoverProbability = Math.random();
-                double mutationProbability = Math.random();
-                // double crossoverProbability = 0.8;
-                // double mutationProbability = 0.1;
+                // double crossoverProbability = Math.random();
+                // double mutationProbability = Math.random();
+                double crossoverProbability = 0.8;
+                double mutationProbability = 0.1;
 
                 // Create an instance of TSPGeneticAlgorithm with the generated parameters
                 TSPGeneticAlgorithm tspGeneticAlgorithm = new TSPGeneticAlgorithm(crossoverProbability, mutationProbability, lines);
